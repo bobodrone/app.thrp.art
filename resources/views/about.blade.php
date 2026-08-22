194a1f6f29f306033b2be3477194d6159e9608e1
BREG <fredric.bergstrom@fullyhub.se>
2026-08-22 18:49:37 +0200

    added gdpr document

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,36 +2,46 @@
     <section class="bg-soil-100 px-5 py-14">
         <div class="mx-auto max-w-6xl">
             <h1 class="mb-1 font-display text-3xl font-black text-soil-900">About</h1>
-            <p class="mb-6 font-body text-sm text-soil-600">
-Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-            </p>
 
             <p class="mb-6 font-body text-sm text-soil-600">
-Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+              The Human Response Project (thrp) is a simple, interactive app that aims to provide a human response to any given query. 
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              We find ourselves surrounded by instantaneous, automated, and artificially enhanced interactions: thrp is the antidote. Thrp is slow moving, thoughtful and endlessly creative. Behind the interface is an evolving and ever-changing bank of human beings who are donating their time and energy to respond to whatever it is that you are asking. The response may not be the answer you were looking for (it may not be an answer at all), but it will be honest, creative and human-made.
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              Responses may take minutes, hours, days or longer. It's an exercise in patience and delayed gratification. 
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              The intention is to provide a creative and artistic response to each query from one or more thrp "Responders". This could be in the form of a poem, sketch, photo, doodle, piece of music, video, spoken word or any manner of interpretation. It may also, simply, be an answer to your question.
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              Thrp is completely dependent upon the generosity of its Responders. We hope that it will be something that artists enjoy taking part in, for as much or as little as they like. It's an opportunity to put intentional art out into the world as a reflection of what's on people's minds. 
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              All artwork and content produced by Responders on this site is released under a Creative Commons license. This license requires that reusers give credit to the creator. It allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, even for commercial purposes. If others remix, adapt, or build upon the material, they must license the modified material under identical terms. https://creativecommons.org/licenses/by-sa/4.0/
+            </p>
+            <p class="mb-6 font-body text-sm text-soil-600">
+              The Human Response Project was conceived by Robin Vincent in May 2026 after a conversation about being ridiculously human in the face of AI. He is immensely grateful to everyone who has contributed and continues to support this project, particularly Pennache who has single-handedly realised the idea into a workable web app. 
             </p>
 
-
-            <h2 class="mb-1 font-display text-2xl font-black text-soil-900">Legal</h1>
-            <p class="mb-6 font-body text-sm text-soil-600">How do we handle GDPR?</p>
-
-
-            <h2 class="mb-1 font-display text-2xl font-black text-soil-900">Credits</h1>
+            <h2 class="mb-1 font-display text-2xl font-black text-soil-900">CREDITS</h1>
             <ul class="list-disc">
-              <li><strong>Concept/Idea:</strong> Robin Vincent</li>
+              <li><strong>Founder:</strong> Founder: Robin Vincent - https://robinvincent.net/</li>
               <li><strong>Design and programming:</strong> Pennache</li>
-              <li><strong>...more:</strong> Who?</li>
+              <li><strong>Join us:</strong> This is intended as a community project that welcomes input, change and evolution from anyone willing to offer their time and talents.</li>
             </ul>
-
-
         </div>
-    </section>
+    </section>  
     <section class="bg-gray-800 px-5 py-4">
         <div class="mx-auto max-w-6xl">
           <p>
             <a
-              href="#"
-              class="text-white underline-offset-4 underline" target="_blank"
-            >Credits and legal information</a>
+              href="{{ asset('docs/Privacy Policy and GDPR Notice.pdf') }}"
+              class="text-white underline-offset-4 underline"
+              target="_blank"
+              rel="noopener"
+            >Privacy Policy and GDPR Notice</a>
           </p>
         </div>
     </section>
